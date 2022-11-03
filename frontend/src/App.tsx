2163384d1f0b1640a7788d621b95a936342b3804
iskander-faggod <kudashev.iskander13@gmail.com>
2022-11-03 17:59:47 +0300

    feat: init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/core/Header';
+
+function App() {
+  return (
+      <>
+          <Header/>
+          <div className="App">
+          </div>
+      </>
+  );
+}
+
+export default App;
---
+++ frontend/src/styles/styledHeader.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+import Box from "@mui/material/Box";
+import {AppBar, Toolbar, Typography} from "@mui/material";
+
+export const HeaderContainer = styled(Box)`
+    display: flex;
+`;
+export const StyledAppBar = styled(AppBar)`
+  height: 60px;
+`;
+
+export const ProjectText = styled(Typography)`
+`;
+
+export const StyledToolbar = styled(Toolbar)`
+    display: flex;
+`
+
+export const ItemsContainer = styled.div`
+    display: flex;
+`
