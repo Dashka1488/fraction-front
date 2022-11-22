f1122ebe329da92dda68f1032b005d6706694a5b
Dasha <fskvortsovf@gmail.com>
2022-11-23 00:38:05 +0300

    fixed contract abi

--- frontend/src/components/core/PoolsContainer.tsx
+++ frontend/src/components/core/PoolsContainer.tsx
@@ -4,28 +4,33 @@ import {NFTContainer, PoolsFlexContainer} from "../../styles/styledPoolsContaine
 import Button from "@mui/material/Button";
 import {useCallback, useEffect, useState} from "react";
 import {ethers} from "ethers";
-import {jsonAbi} from '../../abi';
+import jsonAbi from '../../abi.json';
 
 type BigNumber = {
     _hex : string,
     _isBigNumber: true;
 }
 export const PoolsContainer = () => {
-    const [pools, setPools] = useState(null);
-    const object_abi = JSON.stringify(JSON.parse(jsonAbi), null, 2);
+    const [pools, setPools] = useState<any>(null);
+    const [contract, setContract] = useState<any>(null);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const address = '0x1F5b3a4C463b606615846FE8Dd303a23daa57Fb3';
-    const contract = new ethers.Contract(address, object_abi, provider)
 
-    const getPools = useCallback(async ()=>{
-        const activePools = await contract.getActivePools()
-        setPools(activePools)
-    }, [pools])
+    const getPools = useCallback(async () => {
+        return await contract.functions.getActivePools();
+    }, [contract])
 
-    useEffect(()=>{
-        console.log(contract)
-        console.log(pools)
-    }, [pools, getPools]);
+    useEffect(() => {
+        if (!contract && provider) {
+            setContract(new ethers.Contract(address, jsonAbi, provider));
+        }
+    }, [contract, provider]);
+
+    useEffect(() => {
+        if (contract) {
+            getPools().then(setPools);
+        }
+    }, [contract]);
 
     return (
         <PoolsFlexContainer container>
